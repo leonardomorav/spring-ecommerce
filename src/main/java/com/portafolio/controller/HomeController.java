037ef5c07be6c5f8076da40aba051c23cd6794e1
leonardomorav <leonardomorav@gmail.com>
2024-06-20 18:31:28 +0100

    añadir la funcion de resumen a carrito

--- src/main/java/com/portafolio/controller/HomeController.java
+++ src/main/java/com/portafolio/controller/HomeController.java
@@ -123,4 +123,13 @@ public class HomeController {
     
     return "/usuario/carrito";
     }
+    
+    
+    @GetMapping("/order")
+    public String order(){
+    
+    return "usuario/resumenorden";
+    
+    
+    }
 }
